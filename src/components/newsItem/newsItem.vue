468fb1a2f9ac76e4a0ff8d577ad7858874fd4a99
mazixiao <1598605372@qq.com>
2018-12-22 19:02:34 +0800

    维护

--- src/components/newsItem/newsItem.vue
+++ src/components/newsItem/newsItem.vue
@@ -12,7 +12,7 @@
 		    <scroller lock-x height="480px" @on-scroll-bottom="onScrollBottom" ref="scrollerBottom" :scroll-bottom-offst="480">
 				<div class="newsDetail">
 					<div class="item" v-if="ajaxData" v-for="(item, index) in ajaxData" :key='index'>
-						<a class='tit_s' target="_blank" :href="item.url">{{item.title}}</a>
+						<a class='tit_s' target="_blank" :href="item.url">{{index + 1}}.{{item.title}}</a>
 						<div class="introduct">{{item.content}}</div>
 						<div class="img">
 							<a class='imgs' :href='imgs' target="_blank" v-for="(imgs, index1) in item.imageUrls" :key='index1' :style="{'background-image': 'url(' + imgs + ')'}"></a>
